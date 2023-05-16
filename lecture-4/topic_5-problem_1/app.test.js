d6a736a33996e1acfa97ad6a9dac91556b1a3635
vivek kr <offecho28@gmail.com>
2023-05-16 23:28:56 +0530

    fixed lec-4 topic-4 ques-1

--- lecture-4/topic_5-problem_1/app.test.js
+++ lecture-4/topic_5-problem_1/app.test.js
@@ -1,35 +1,21 @@
 const request = require("supertest");
 const express = require("express");
 const setCustomHeader = require(".");
+
 describe("GET / - setCustomHeader", () => {
   let app;
-
-  beforeEach(() => {
-    app = express();
-    jest.spyOn(console, "log").mockImplementation();
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
+  app = express();
 
   it("sets the custom header and sends the response", async () => {
-
-
     app.get("/", (req, res) => {
-      setCustomHeader(res, "Content-Type", "application/json");
-      res.json("get method called!");
+      setCustomHeader(res, "Content-Type", "text/plain");
+      res.send("get method called!");
     });
 
     const response = await request(app).get("/");
 
     expect(response.status).toBe(200);
-    // expect(response.text).toBe("get method called!");
-    // expect(response.headers["content-type"]).toBe("application/json");
-    expect(console.log).toHaveBeenCalledWith(
-      "Content-Type with value application/json has been set successfully!"
-    );
+    expect(response.text).toBe("get method called!");
+    expect(response.headers["content-type"]).toMatch(/text/);
   });
-
-  // Add more test cases if needed
 });
