c3137385f36e555ec585bcea69b9d00d9496ac94
vivek kr <offecho28@gmail.com>
2023-06-03 17:46:08 +0530

    added lec-3 test codes

---
+++ lecture-1/topic_9-problem_1/defaultcode/index.js
@@ -0,0 +1,8 @@
+// Please do not change the prewritten code
+
+const http = require("http");
+
+const server = http.createServer(function requestListner(request, response) {
+  //write your code here
+});
+module.exports = server;
