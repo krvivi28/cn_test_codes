cd3034990469cd3556686116657ee3f9837d66ac
vivek <offecho28@gmail.com>
2023-06-10 01:02:05 +0530

    added lec-6 topic-4 problem

--- lecture-6/topic-4/solution/index.js
+++ lecture-6/topic-4/solution/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { newUser } from "./user.controller.js";
+import { formValidationMiddleware } from "./middleware.js";
 
 const app = express();
 app.use(express.json());
-app.post("/new", newUser);
+app.post("/new", formValidationMiddleware, newUser);
 
 export default app;
